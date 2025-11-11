a3a8759a16e7bf53f51b8f15503ebffa2a12f045
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-11-12 00:35:57 +0100

    relay.py: reduce output

--- nostr/relay.py
+++ nostr/relay.py
@@ -150,7 +150,7 @@ class Relay:
         pass
 
     def _on_message(self, class_obj, message: str):
-        print(f"relay.py _on_message received: {message}")
+        print(f"relay.py _on_message received message of length {len(message)}: {message[:120]}...")
         if self._is_valid_message(message):
             self.num_received_events += 1
             self.message_pool.add_message(message, self.url)
